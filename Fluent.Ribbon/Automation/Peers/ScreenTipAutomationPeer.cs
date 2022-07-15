39987cacc1151da3b7ab3f8ae14e5740b419d222
cbra_caa <caa@cbrain.com>
2022-07-15 14:33:03 +0200

    Fix that screentip automationpeer was not providing anything. Has become relevant after the latest WPF update were keyboard focus also activates the tooltip.
    7.0.0-70

--- Fluent.Ribbon/Automation/Peers/ScreenTipAutomationPeer.cs
+++ Fluent.Ribbon/Automation/Peers/ScreenTipAutomationPeer.cs
@@ -1,22 +1,63 @@
 ﻿namespace Fluent.Automation.Peers
 {
-    using System.Windows;
-    using System.Windows.Automation;
+    using System;
     using System.Windows.Automation.Peers;
-    using System.Windows.Automation.Provider;
     using JetBrains.Annotations;
 
     /// <summary>
-    ///     Automation peer for <see cref="ScreenTip" />.
+    /// Automation peer for <see cref="Fluent.ScreenTip" />.
     /// </summary>
     public class ScreenTipAutomationPeer : ToolTipAutomationPeer
     {
+        private ScreenTip ScreenTip => (ScreenTip)this.Owner;
+
         /// <summary>
         ///     Creates a new instance.
         /// </summary>
         public ScreenTipAutomationPeer([NotNull] ScreenTip owner)
             : base(owner)
         {
+        }
+
+        /// <inheritdoc />
+        protected override string GetNameCore()
+        {
+            string name = base.GetNameCore();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                name = this.ScreenTip.Title;
+            }
+
+            return name;
+        }
+
+        /// <inheritdoc />
+        protected override string GetHelpTextCore()
+        {
+            string helpText = base.GetHelpTextCore();
+
+            if (string.IsNullOrEmpty(helpText))
+            {
+                string screenTipHelpText = this.ScreenTip.Text;
+
+                if (!string.IsNullOrEmpty(this.ScreenTip.DisableReason))
+                {
+                    string disableReason = RibbonLocalization.Current.Localization.ScreenTipDisableReasonHeader
+                        + Environment.NewLine + this.ScreenTip.DisableReason;
+
+                    screenTipHelpText += Environment.NewLine + disableReason;
+                }
+
+                if (this.ScreenTip.HelpTopic != null)
+                {
+                    screenTipHelpText += Environment.NewLine + RibbonLocalization.Current.Localization.ScreenTipF1LabelHeader;
+                }
+
+                return screenTipHelpText;
+            }
+
+            return helpText;
         }
 
         /// <inheritdoc />
